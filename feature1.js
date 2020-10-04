0a7891cc7d127437e295d603b0a91a47a4d5445e
Hadiest <thanh.trung.4497@gmail.com>
2020-10-04 11:41:20 +0700

    em yeu vi

--- feature1.js
+++ feature1.js
@@ -2,5 +2,5 @@ console.log("feature1 hehehehehehehe");
 console.log("feature1 hehehehehehehe");
 console.log("feature1 hehehehehehehe");
 console.log("feature1 hehehehehehehe");
-console.log("feature1 hehehehehehehe");
+console.log("feature1 con cac");
 console.log("feature1 hehehehehehehe");
